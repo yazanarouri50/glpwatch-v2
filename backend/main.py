bd518f5b9a65b5b2665a32f23512b41b2bef7eca
Yazan Arouri <yazanarouri50@gmail.com>
2026-03-13 16:49:48 +1030

    Fix CORS for GitHub Pages

--- backend/main.py
+++ backend/main.py
@@ -51,9 +51,9 @@ app.add_middleware(
         "http://localhost:3000",
         "http://localhost:8080",
         "http://127.0.0.1:8080",
-        "http://localhost:5500",   # VS Code Live Server
+        "http://localhost:5500",
         "http://127.0.0.1:5500",
-        "https://yazanarouri50.github.io",
+        "https://yazanarouri50.github.io",  
     ],
     allow_methods=["GET"],
     allow_headers=["*"],
